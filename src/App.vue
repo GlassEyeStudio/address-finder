97fa8f4cfab2a80d2a585c09fc7ca83b8a5954dd
GlassEyeStudio <dawid@witulski.org>
2020-08-31 12:32:04 +0200

    Added: initial csv parse functionalities

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <CsvImporter />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+  import CsvImporter from "@/components/CsvImporter";
 
-export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
-};
+  export default {
+    name: "App",
+    components: {
+      CsvImporter
+    }
+  };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/CsvImporter.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="wrapper">
+    <vue-csv-import
+      v-model="parseCsv"
+      :autoMatchFields="true"
+      :autoMatchIgnoreCase="true"
+      :map-fields="{
+        city: 'City',
+        state: 'State',
+        zipCode: 'Zip-code',
+        address: 'Address',
+        category: 'Category'
+      }"
+      url="/"
+    >
+      <template slot="hasHeaders" slot-scope="{headers, toggle}">
+        <label>
+          <input type="checkbox" id="hasHeaders" :value="headers" @change="toggle">
+          Headers?
+        </label>
+      </template>
+
+      <template slot="thead">
+        <tr>
+          <th>My Fields</th>
+          <th>Column</th>
+        </tr>
+      </template>
+
+      <template slot="next" slot-scope="{load}">
+        <button @click.prevent="load">load!</button>
+      </template>
+
+      <template slot="submit" slot-scope="{submit}">
+        <button @click.prevent="submit">send!</button>
+      </template>
+    </vue-csv-import>
+  </div>
+</template>
+
+<script>
+  import { VueCsvImport } from "vue-csv-import";
+
+  export default {
+    name: "CsvImporter",
+    components: { VueCsvImport },
+    data() {
+      return {
+        parseCsv: []
+      };
+    }
+  };
+</script>
+
+<style scoped></style>
