6e4983ddfb246833d08ac450ff8f8263927ac931
GlassEyeStudio <dawid@witulski.org>
2020-08-31 19:39:25 +0200

    Added: categories colors

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -13,6 +13,7 @@
           size="small"
           @click="toggleFilter(category.name)"
           :type="filters.includes(category.name) ? 'primary' : 'plain'"
+          :style="'border-color:' + category.color"
         >
           {{ category.name }}
         </el-button>
@@ -51,7 +52,9 @@
           v-for="(point, index) in filteredList"
           :key="index"
           :id="point.category"
+          :icon="getIcon(categories.find(i => i.name === point.category).color)"
         >
+          <l-icon-default class-name="someExtraClass" />
           <l-popup>
             <span v-html="point.name" />
           </l-popup>
@@ -62,8 +65,14 @@
 </template>
 
 <script>
-  import { latLng, latLngBounds } from "leaflet";
-  import { LMap, LTileLayer, LMarker, LPopup } from "vue2-leaflet";
+  import { divIcon, latLng, latLngBounds } from "leaflet";
+  import {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LPopup,
+    LIconDefault
+  } from "vue2-leaflet";
 
   /* Missing markers fix */
   import { Icon } from "leaflet";
@@ -82,7 +91,8 @@
       LMap,
       LTileLayer,
       LMarker,
-      LPopup
+      LPopup,
+      LIconDefault
     },
     data() {
       return {
@@ -134,7 +144,8 @@
             this.categories.push({
               name: i,
               value: this.$store.state.markers.filter(j => j.category === i)
-                .length
+                .length,
+              color: this.getRandomColor()
             });
           }
         );
@@ -161,6 +172,29 @@
           this.filteredList = this.$store.state.markers.filter(j =>
             this.filters.includes(j.category)
           );
+      },
+
+      // Returns custom icon;
+      getIcon(color) {
+        return divIcon({
+          className: "my-custom-pin",
+          html: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 34.892337" height="60" width="40">
+    <g transform="translate(-814.59595,-274.38623)">
+      <g transform="matrix(1.1855854,0,0,1.1855854,-151.17715,-57.3976)">
+        <path d="m 817.11249,282.97118 c -1.25816,1.34277 -2.04623,3.29881 -2.01563,5.13867 0.0639,3.84476 1.79693,5.3002 4.56836,10.59179 0.99832,2.32851 2.04027,4.79237 3.03125,8.87305 0.13772,0.60193 0.27203,1.16104 0.33416,1.20948 0.0621,0.0485 0.19644,-0.51262 0.33416,-1.11455 0.99098,-4.08068 2.03293,-6.54258 3.03125,-8.87109 2.77143,-5.29159 4.50444,-6.74704 4.56836,-10.5918 0.0306,-1.83986 -0.75942,-3.79785 -2.01758,-5.14062 -1.43724,-1.53389 -3.60504,-2.66908 -5.91619,-2.71655 -2.31115,-0.0475 -4.4809,1.08773 -5.91814,2.62162 z" style="fill:${color};stroke:${color};"/>
+        <circle r="3.0355" cy="288.25278" cx="823.03064" id="path3049" style="display:inline;fill:white;"/>
+      </g>
+    </g>
+  </svg>`
+        });
+      },
+      getRandomColor() {
+        const letters = "0123456789ABCDEF";
+        let color = "#";
+        for (let i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
       }
     }
   };
