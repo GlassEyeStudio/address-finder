f880e09c29d2e640c3c297cc3487ce866eeae25c
GlassEyeStudio <dawid@witulski.org>
2020-09-01 08:45:54 +0200

    Added: E2E tests

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,21 +1,41 @@
-// For authoring Nightwatch tests, see
-// https://nightwatchjs.org/guide
+// Basic test for initial screen
 
 module.exports = {
-  "default e2e tests": browser => {
+  "init screen": browser => {
     browser
       .init()
       .waitForElementVisible("#app")
-      .assert.elementPresent(".hello")
-      .assert.containsText("h1", "Welcome to Your Vue.js App")
-      .assert.elementCount("img", 1)
+      .assert.elementPresent("h1")
+      .assert.containsText("h1", "Find Your Addresses!")
+      .assert.elementPresent(".el-steps")
+      .assert.elementCount("input[type=file]", 1)
+      .assert.elementCount("button", 1)
+      .assert.elementPresent(".instruction")
       .end();
   },
 
-  "example e2e test using a custom command": browser => {
+  "full flow checkup": browser => {
     browser
-      .openHomepage()
-      .assert.elementPresent(".hello")
+      .init()
+      .setValue(
+        "input[type=file]",
+        require("path").resolve(__dirname + "/dummy-files/input.csv")
+      )
+      .click(".vue-csv-uploader-part-one button")
+      .assert.elementPresent("table")
+      .assert.elementCount("button", 2)
+      .pause(100)
+      .click("select[name='csv_uploader_map_2'] option:nth-child(3)")
+      .pause(100)
+      .click(".vue-csv-uploader-part-two button")
+      .pause(300)
+      .assert.elementPresent(".el-progress-circle")
+      .pause(1500)
+      .assert.elementPresent(".vue2leaflet-map")
+      .assert.elementCount(".el-table tr", 8)
+      .assert.elementCount(".el-badge", 4)
+      .assert.elementCount(".my-custom-pin", 7)
+      .assert.elementPresent("button.floating")
       .end();
   }
 };
